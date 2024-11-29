a29b2c0fce0081588950682ebc95e7de729874bb
MK Abdul Azees <115166340+mkabdulazees56@users.noreply.github.com>
2024-11-29 08:15:03 +0530

    vercel setup

--- frontend/todo/src/services/AxiosInsance.jsx
+++ frontend/todo/src/services/AxiosInsance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance  = axios.create({
-    baseURL: "http://127.0.0.1:5000/api/v1",
+    baseURL: "flask-react-todoapp-amber.vercel.app/api/v1",
     headers: {
         "Content-Type": "application/json"
     },
